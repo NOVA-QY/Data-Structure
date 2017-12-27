c6d4371f131b65f5d7933ef0d402776c5466599f
Homo_sapiens <820043860@qq.com>
2017-12-27 16:18:19 +0800

    图模板测试代码

---
+++ 07.Graph/ReadGraph_model_test.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+#include "SparseGraph_model.h"
+#include "DenseGraph_model.h"
+#include "ReadGraph_model.h"
+
+using namespace std;
+
+int main(){
+	
+	string filename="testG1.txt";
+	SparseGraph g1(13,false);
+	ReadGraph<SparseGraph> readGraph(g1,filename);
+	g1.show();
+	
+	return 0;
+}
