7826b57d49c1eeaa17df24217a640eead441103a
Homo_sapiens <820043860@qq.com>
2017-12-28 15:17:28 +0800

    测试图模板

--- 07.Graph/ReadGraph_model_test.cpp
+++ 07.Graph/ReadGraph_model_test.cpp
@@ -9,8 +9,14 @@ int main(){
 	
 	string filename="testG1.txt";
 	SparseGraph g1(13,false);
-	ReadGraph<SparseGraph> readGraph(g1,filename);
+	ReadGraph<SparseGraph> readGraph1(g1,filename);
 	g1.show();
+	
+	cout<<endl;
+	
+	DenseGraph g2(13,false);
+	ReadGraph<DenseGraph> readGraph2(g2,filename);
+	g2.show();
 	
 	return 0;
 }
