edc728c7c42986d3b6960ccc55495e1e857095d2
Homo_sapiens <820043860@qq.com>
2017-12-30 20:14:48 +0800

    【测试】最小生成树

---
+++ 08.MinimumSpanTree/LazyPrimMST_model_test.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <iomanip>
+#include "DenseGraph_model.h"
+#include "SparseGraph_model.h"
+#include "ReadGraph_model.h"
+#include "LazyPrimMST_model.h"
+
+using namespace std;
+
+// 测试最小生成树算法
+int main() {
+
+    string filename = "testG3.txt";
+    int V = 8;
+
+    SparseGraph<double> g = SparseGraph<double>(V, false);
+    ReadGraph<SparseGraph<double>, double> readGraph(g, filename);
+
+    // Test Lazy Prim MST
+    cout<<"Test Lazy Prim MST:"<<endl;
+    LazyPrimMST<SparseGraph<double>, double> lazyPrimMST(g);
+    vector<Edge<double> > mst = lazyPrimMST.mstEdges();
+    for( int i = 0 ; i < mst.size() ; i ++ )
+        cout<<mst[i]<<endl;
+    cout<<"The MST weight is: "<<lazyPrimMST.result()<<endl;
+
+    cout<<endl;
+
+    return 0;
+}
